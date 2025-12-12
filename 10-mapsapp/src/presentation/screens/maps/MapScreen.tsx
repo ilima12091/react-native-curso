a4913b744aca063542484c01244bfb296d59b65d
ilima12091 <limaignacio12091@gmail.com>
2025-12-11 21:21:22 -0300

    Se agrega la carga del mapa

--- 10-mapsapp/src/presentation/screens/maps/MapScreen.tsx
+++ 10-mapsapp/src/presentation/screens/maps/MapScreen.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
 export const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFill,
+  },
+  map: {
+    ...StyleSheet.absoluteFill,
+  },
+});
